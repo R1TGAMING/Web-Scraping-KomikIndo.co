1ea4de201a4115469ab138a3bf4e482ffc311617
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-05-24 13:01:18 +0000

    manga read 40%

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-res = requests.get("https://komikindo.co/one-piece-chapter-1/")
+###res = requests.get("https://komikindo.co/solo-leveling-side-story-chapter-01/")
 
-soup = BeautifulSoup(res.content, "html.parser")
+"""soup = BeautifulSoup(res.content, "html.parser")
 list = []
 
 for tag in soup.find_all("img", {"data-tai" : True}):
@@ -16,4 +16,27 @@ for tag in soup.find_all("img", {"data-tai" : True}):
 
 with open(f"./data.json", "w") as f :
   print("success")
-  json.dump(list, f, indent = 4)
+  json.dump(list, f, indent = 4)"""
+  
+def manga_read(name : str) :
+  split = name.split()
+  join = "-".join(split)
+  res =  requests.get(f"https://komikindo.co/manga/{join}/")
+  data = []
+  soup = BeautifulSoup(res.text, "html.parser")
+  def get_total_chapter() :
+    
+     for i in soup.find_all("li", {"data-num" : True}):
+       find_chapter = i.find("a", {"href" : True})
+       jsons = {
+         "total_chapter" : find_chapter.text
+       }
+       return jsons
+  jsons = {
+    "title" : soup.find("h1", class_="entry-title").text,
+    "statistic" : get_total_chapter()
+  }
+  data.append(jsons)
+  with open("data.json", "w") as f :
+    json.dump(data, f, indent=4)
+manga_read("one piece")
