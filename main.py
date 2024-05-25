d5dbc35e8f6c787030160c90ad7cba3114b00ad6
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-05-25 09:26:23 +0000

    handle error

--- main.py
+++ main.py
@@ -19,13 +19,14 @@ with open(f"./data.json", "w") as f :
   json.dump(list, f, indent = 4)"""
   
 def get_manga(name : str) :
-  split = name.split()
-  join = "-".join(split)
-  res =  requests.get(f"https://komikindo.co/manga/{join}/")
-  data = []
-  soup = BeautifulSoup(res.text, "html.parser")
+  try : 
+   split = name.split()
+   join = "-".join(split)
+   res =  requests.get(f"https://komikindo.co/manga/{join}/")
+   data = []
+   soup = BeautifulSoup(res.text, "html.parser")
 
-  def get_total_chapter() :
+   def get_total_chapter() :
     
      for i in soup.find_all("div", {"id" : "chapterlist"}):
        
@@ -35,7 +36,7 @@ def get_manga(name : str) :
        }
        return jsons
 
-  def get_description() :
+   def get_description() :
     get_data = soup.find("tbody").find_all("td")
     komikindo = soup.find("i", {"itemprop" : "name"})
     desc = soup.find("div", {"itemprop" : "description"})
@@ -52,39 +53,49 @@ def get_manga(name : str) :
     
     
 
-  jsons = {
+   jsons = {
     "url" : res.url,
     "title" : soup.find("h1", class_="entry-title").text,
     "description" : get_description(),
     "statistic" : get_total_chapter()
   }
-  data.append(jsons)
-  with open("data.json", "w") as f :
+   data.append(jsons)
+   with open("data.json", "w") as f :
     json.dump(data, f, indent=4)
+    if data == [] :
+      print("Error Manga Tidak Ditemukan")
+    else :
+      print("Scraping Manga Success")
+  except Exception as e :
+    print("Error Manga Tidak Ditemukan")
 
-def get_chapter(name : str, chapter : str) :
-  split2 = chapter.split()
-  join2 = "-".join(split2)
-  split = name.split()
-  join = "-".join(split)
-  data = []
-  res =  requests.get(f"https://komikindo.co/{join}-{join2}/")
-  print(res.url)
-  soup = BeautifulSoup(res.content, "html.parser")
-  for i in soup.find_all("img", {"data-tai" : True}) :
-    jsons = {
+def get_chapter(name : str, chapter : int) :
+  try :
+   split = name.split()
+   join = "-".join(split)
+   data = []
+   res =  requests.get(f"https://komikindo.co/{join}-chapter-{str(chapter)}/")
+   soup = BeautifulSoup(res.content, "html.parser")
+   for i in soup.find_all("img", {"data-tai" : True}) :
+    
+     jsons = {
       "src" : i["src"],
       "page" : i["data-tai"]
     }
-    data.append(jsons)
-  with open("get_chapter.json", "w") as f :
+     data.append(jsons)
+   with open("get_chapter.json", "w") as f :
     json.dump(data, f, indent=4)
-    print("success")
+    if data == [] :
+      print("Error Chapter Tidak Ditemukan")
+    else :
+      print("Scraping Chapter Success")
+  except Exception as e :
+    print("Error ", e)
     
     
-def all_manga(name : str, chapter : str) :
+def all_manga(name : str, chapter : int) :
   get_chapter(name, chapter)
   get_manga(name)
 
   
-all_manga("one piece", "chapter 1115")
+all_manga("one piece", 120)
